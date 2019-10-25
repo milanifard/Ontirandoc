b5c8623b9dae4de86e09f06f22e8c81cd43b1648
omid <milanifard.o@gmail.com>
2019-10-25 16:45:00 +0330

    adding some tags in header for bootstrap support and a key for UI language

--- www/shares/header.inc.php
+++ www/shares/header.inc.php
@@ -3,12 +3,30 @@
 	include "DateUtils.inc";
 	include "SharedClass.class.php";
 
-	function HTMLBegin($bgcolor = '#C8DEF0') 
-	{
-		echo '<style type="text/css" > INPUT, SELECT { font-family: Tahoma }'.
-			'</style>'.
-			'<link rel="stylesheet"  href="css/login.css" type="text/css">'.
-			"<body  dir=rtl link=\"#0000FF\" alink=\"#0000FF\" vlink=\"#0000FF\">";
+	function HTMLBegin($bgcolor = '#C8DEF0')
+    {
+        echo "<!DOCTYPE html>\n<html>\n<head>\n" .
+
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n".
+        "<link rel=\"stylesheet\"  href=\"css/login.css\" type=\"text/css\">\n";
+        if (UI_LANGUAGE == "EN") {
+            echo "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n".
+                "<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n".
+                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n".
+                "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>\n";
+        }
+        else if (UI_LANGUAGE == "FA")
+        {
+            echo "<link rel=\"stylesheet\"  href=\"https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css\" integrity=\"sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If\"  crossorigin=\"anonymous\">\n" .
+            "<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n".
+            "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n".
+            "<script  src=\"https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js\" integrity=\"sha384-a9xOd0rz8w0J8zqj1qJic7GPFfyMfoiuDjC9rqXlVOcGO/dmRqzMn34gZYDTel8k\" crossorigin=\"anonymous\"></script>\n";
+        }
+        echo "\n</head>\n";
+        echo "<body ";
+        if(UI_LANGUAGE=="FA")
+            echo " dir = 'rtl' ";
+        echo ">";
 	}
 	
 	function HTMLEnd()
--- www/shares/sys_config.class.php
+++ www/shares/sys_config.class.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'config.class.php';
+define("UI_LANGUAGE", "FA"); // EN or FA
 class sys_config{
 	 public static $db_server = array (
 	          "driver"   => "",
