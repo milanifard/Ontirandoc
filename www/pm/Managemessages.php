33f0ba09270471a04a0aa9092a9b06ff58b24793
kourosh <kouroshinfo96@gmail.com>
2020-01-19 10:37:17 +0330

    Managemessages.php fully bootstrap and bugs fixed

--- www/pm/Managemessages.php
+++ www/pm/Managemessages.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<html>
+<body class="bg-transparent">
 <?php
 /*
  صفحه  نمایش لیست و مدیریت داده ها مربوط به : پیامها
@@ -110,8 +113,10 @@ if(isset($_REQUEST["UpdateID"]))
     }
 }
 ?>
-<form method="post" id="f1" name="f1" enctype="multipart/form-data" >
-    <?
+<div class="container ">
+    <div class="col-md-12">
+        <form method="post" id="f1" name="f1" enctype="multipart/form-data" >
+    <?php
     if(isset($_REQUEST["UpdateID"]))
     {
         echo "<input type=\"hidden\" name=\"UpdateID\" id=\"UpdateID\" value='".$_REQUEST["UpdateID"]."'>";
@@ -119,19 +124,19 @@ if(isset($_REQUEST["UpdateID"]))
     ?>
 
 
-    <br><table width="90%" border="1" cellspacing="0" align="center">
+    <br><table width="90%" class="table-bordered" cellspacing="0" align="center">
         <tr class="HeaderOfTable">
-            <td align="center"><?php echo CREATE_EDIT ?></td>
+            <td align="center" class="table-info"><?php echo CREATE_EDIT ?></td>
         </tr>
         <tr>
             <td>
-                <table width="100%" border="0">
+                <table width="100%">
                     <tr>
                         <td width="1%" nowrap>
                             <?php echo C_MESSAGE?>
                         </td>
                         <td nowrap>
-                            <textarea name="Item_MessageBody" id="Item_MessageBody" cols="80" rows="5"><? echo $TMessageBody ?></textarea>
+                            <textarea name="Item_MessageBody" id="Item_MessageBody" cols="80" rows="5"><?php echo $TMessageBody ?></textarea>
                         </td>
                     </tr>
                     <tr>
@@ -140,9 +145,9 @@ if(isset($_REQUEST["UpdateID"]))
                         </td>
                         <td nowrap>
                             <input type="file" name="Item_FileContent" id="Item_FileContent">
-                            <? if(isset($_REQUEST["UpdateID"]) && $obj->RelatedFileName!="") { ?>
-                                <a href='DownloadFile.php?FileType=messages&RecID=<? echo $_REQUEST["UpdateID"]; ?>'><?php echo REC_FILE?>[<?php echo $obj->RelatedFileName; ?>]</a>
-                            <? } ?>
+                            <?php if(isset($_REQUEST["UpdateID"]) && $obj->RelatedFileName!="") { ?>
+                                <a href='DownloadFile.php?FileType=messages&RecID=<?php echo $_REQUEST["UpdateID"]; ?>'><?php echo REC_FILE?>[<?php echo $obj->RelatedFileName; ?>]</a>
+                            <?php } ?>
                         </td>
                     </tr>
                     <tr>
@@ -151,9 +156,9 @@ if(isset($_REQUEST["UpdateID"]))
                         </td>
                         <td nowrap>
                             <input type="file" name="Item_ImageFileContent" id="Item_ImageFileContent">
-                            <? if(isset($_REQUEST["UpdateID"]) && $obj->ImageFileName!="") { ?>
+                            <?php if(isset($_REQUEST["UpdateID"]) && $obj->ImageFileName!="") { ?>
                                 <img width=50 src='ShowMessagePhoto.php?MessageID=<? echo $_REQUEST["UpdateID"]; ?>'>
-                            <? } ?>
+                            <?php } ?>
                         </td>
                     </tr>
                     <tr>
@@ -161,9 +166,9 @@ if(isset($_REQUEST["UpdateID"]))
                             <?php echo START_TIME?>
                         </td>
                         <td nowrap>
-                            <input maxlength="2" id="StartDate_DAY"  name="StartDate_DAY" type="text" size="2">/
-                            <input maxlength="2" id="StartDate_MONTH" name="StartDate_MONTH" type="text" size="2" >/
-                            <input maxlength="2" id="StartDate_YEAR" name="StartDate_YEAR" type="text" size="2" >
+                            <input type="date"  id="StartDate" name="StartDate" size="2">
+                           <!-- <input maxlength="2" id="StartDate_MONTH" name="StartDate_MONTH" type="text" size="2" >/-->
+                          <!--  <input maxlength="2" id="StartDate_YEAR" name="StartDate_YEAR" type="text" size="2" >-->
                         </td>
                     </tr>
                     <tr>
@@ -171,24 +176,26 @@ if(isset($_REQUEST["UpdateID"]))
                             <?php echo END_TIME?>
                         </td>
                         <td nowrap>
-                            <input maxlength="2" id="EndDate_DAY"  name="EndDate_DAY" type="text" size="2">/
-                            <input maxlength="2" id="EndDate_MONTH" name="EndDate_MONTH" type="text" size="2" >/
-                            <input maxlength="2" id="EndDate_YEAR" name="EndDate_YEAR" type="text" size="2" >
+                            <input type="date"  id="EndDate" name="EndDate">
+                            <!--<input maxlength="2" id="EndDate_MONTH" name="EndDate_MONTH" type="text" size="2" >/-->
+                            <!--<input maxlength="2" id="EndDate_YEAR" name="EndDate_YEAR" type="text" size="2" >-->
                         </td>
                     </tr>
                 </table>
             </td>
         </tr>
         <tr class="FooterOfTable">
-            <td align="center">
-                <input type="button" onclick="javascript: ValidateForm();" value="<?php echo SAVE_M?>">
-                <input type="button" onclick="javascript: document.location='Managemessages.php';" value="<?php echo NEW_M?>">
+            <td align="center" style="border: none" >
+                <input type="button"  class="btn btn-outline-success" onclick="javascript: ValidateForm();" value="<?php echo SAVE_M?>">
+                <input type="button"  class="btn btn-outline-warning"onclick="javascript: document.location='Managemessages.php';" value="<?php echo NEW_M?>">
             </td>
         </tr>
     </table>
     <input type="hidden" name="Save" id="Save" value="1">
-</form><script>
-    <? echo $LoadDataJavascriptCode; ?>
+</form>
+    </div>
+<script>
+    <?php echo $LoadDataJavascriptCode; ?>
     function ValidateForm()
     {
         document.f1.submit();
@@ -242,12 +249,13 @@ for($k=0; $k<count($res); $k++)
 if($SomeItemsRemoved)
     $res = manage_messages::Search($MessageBody, $CreateDate, "", $FromRec, $NumberOfRec, $OrderByFieldName, $OrderType);
 ?>
-<form id="SearchForm" name="SearchForm" method=post>
+    <div class="col-md-12">
+        <form id="SearchForm" name="SearchForm" method=post>
     <input type="hidden" name="PageNumber" id="PageNumber" value="0">
     <input type="hidden" name="OrderByFieldName" id="OrderByFieldName" value="<? echo $OrderByFieldName; ?>">
     <input type="hidden" name="OrderType" id="OrderType" value="<? echo $OrderType; ?>">
     <input type="hidden" name="SearchAction" id="SearchAction" value="1">
-    <br><table width="90%" align="center" border="1" cellspacing="0">
+    <br><table width="90%" align="center" class="table table-borderless" cellspacing="0">
         <tr class="HeaderOfTable">
             <td><img src='images/search.gif'><b><a href="#"  onclick='javascript: if(document.getElementById("SearchTr").style.display=="none") document.getElementById("SearchTr").style.display=""; else document.getElementById("SearchTr").style.display="none";'>
 
@@ -255,7 +263,7 @@ if($SomeItemsRemoved)
         </tr>
         <tr id='SearchTr' style='display: none'>
             <td>
-                <table width="100%" align="center" border="0" cellspacing="0">
+                <table width="100%" align="center" class="table table-bordered" cellspacing="0">
                     <tr>
                         <td width="1%" nowrap>
                             <?php echo C_MESSAGE?>
@@ -274,67 +282,70 @@ if($SomeItemsRemoved)
         </tr>
     </table>
 </form>
-<?
+    </div>
+<?php
 if(isset($_REQUEST["SearchAction"]))
 {
     ?>
     <script>
-        document.SearchForm.Item_MessageBody.value='<? echo htmlentities($_REQUEST["Item_MessageBody"], ENT_QUOTES, 'UTF-8'); ?>';
-        document.SearchForm.Item_CreateDate.value='<? echo htmlentities($_REQUEST["Item_CreateDate"], ENT_QUOTES, 'UTF-8'); ?>';
+        document.SearchForm.Item_MessageBody.value='<?php echo htmlentities($_REQUEST["Item_MessageBody"], ENT_QUOTES, 'UTF-8'); ?>';
+        document.SearchForm.Item_CreateDate.value='<?php echo htmlentities($_REQUEST["Item_CreateDate"], ENT_QUOTES, 'UTF-8'); ?>';
     </script>
-    <?
+    <?php
 }
 ?>
-<form id="ListForm" name="ListForm" method="post">
-    <? if(isset($_REQUEST["PageNumber"]))
+    <div class="col-md-12">
+        <form id="ListForm" name="ListForm" method="post">
+    <?php if(isset($_REQUEST["PageNumber"]))
         echo "<input type=\"hidden\" name=\"PageNumber\" value=".$_REQUEST["PageNumber"].">"; ?>
-    <br><table width="90%" align="center" border="1" cellspacing="0">
-        <tr bgcolor="#cccccc">
-            <td colspan="10">
-                <?php echo MESSAGES_M?>
+            <div class="row">
+                <br><table width="90%" align="center" class="table table-bordered" cellspacing="0">
+                    <tr bgcolor="#cccccc" class="table-info">
+                    <td colspan="10">
+                      <?php echo MESSAGES_M?>
             </td>
+                    </tr>
+                    <tr class="HeaderOfTable">
+            <td width="1%" scope="col"> </td>
+            <td width="1%"scope="col" ><?php echo ROW_M?></td>
+            <td width="2%"scope="col"><?php echo EDIT_M?></td>
+            <td><a href="javascript: Sort('MessageBody', 'ASC');"  scope="col"><?php echo C_MESSAGE?></a></td>
+            <td scope="col"><?php echo AT_FILE?></td>
+            <td scope="col"><?php echo PIC?></td>
+            <td scope="col"><a href="javascript: Sort('CreatorID', 'ASC');"><?php echo CREATOR_M?></a></td>
+            <td scope="col"><a href="javascript: Sort('CreateDate', 'ASC');"><?php echo CREATE_TIM_M?></a></td>
+            <td scope="col"><a href="javascript: Sort('StartDate', 'ASC');"><?php echo START_TIME?></a></td>
+            <td scope="col"><a href="javascript: Sort('EndDate', 'ASC');"><?php echo END_TIME?></a></td>
         </tr>
-        <tr class="HeaderOfTable">
-            <td width="1%"> </td>
-            <td width="1%"><?php echo ROW_M?></td>
-            <td width="2%"><?php echo EDIT_M?></td>
-            <td><a href="javascript: Sort('MessageBody', 'ASC');"><?php echo C_MESSAGE?></a></td>
-            <td><?php echo AT_FILE?></td>
-            <td><?php echo PIC?></td>
-            <td><a href="javascript: Sort('CreatorID', 'ASC');"><?php echo CREATOR_M?></a></td>
-            <td><a href="javascript: Sort('CreateDate', 'ASC');"><?php echo CREATE_TIM_M?></a></td>
-            <td><a href="javascript: Sort('StartDate', 'ASC');"><?php echo START_TIME?></a></td>
-            <td><a href="javascript: Sort('EndDate', 'ASC');"><?php echo END_TIME?></a></td>
-        </tr>
-        <?
+        <?php
         for($k=0; $k<count($res); $k++)
         {
             if($k%2==0)
-                echo "<tr class=\"OddRow\">";
+                echo "<tr scope=\"row\">";
             else
-                echo "<tr class=\"EvenRow\">";
-            echo "<td>";
+                echo "<tr scope=\"row\">";
+            echo "<td scope=\"col\">";
             echo "<input type=\"checkbox\" name=\"ch_".$res[$k]->MessageID."\">";
-            echo "</td>";
-            echo "<td>".($k+$FromRec+1)."</td>";
-            echo "	<td><a href=\"Managemessages.php?UpdateID=".$res[$k]->MessageID."\"><img src='images/edit.gif' title='ویرایش'></a></td>";
-            echo "	<td>".str_replace("\r", "<br>", htmlentities($res[$k]->MessageBody, ENT_QUOTES, 'UTF-8'))."</td>";
-            echo "	<td><a href='DownloadFile.php?FileType=messages&RecID=".$res[$k]->MessageID."'><img src='images/Download.gif'></a></td>";
-            echo "	<td><a target=_blank href='ShowMessagePhoto.php?MessageID=".$res[$k]->MessageID."'><img src='ShowMessagePhoto.php?MessageID=".$res[$k]->MessageID."' width=50></a></td>";
-            echo "	<td>".$res[$k]->CreatorID_FullName."</td>";
-            echo "	<td>".$res[$k]->CreateDate_Shamsi."</td>";
-            echo "	<td>".$res[$k]->StartDate_Shamsi."</td>";
-            echo "	<td>".$res[$k]->EndDate_Shamsi."</td>";
+            echo "</td scope=\"col\">";
+            echo "<td scope=\"col\">".($k+$FromRec+1)."</td>";
+            echo "	<td scope=\"col\"><a href=\"Managemessages.php?UpdateID=".$res[$k]->MessageID."\"><img src='images/edit.gif' title='ویرایش'></a></td>";
+            echo "	<td scope=\"col\">".str_replace("\r", "<br>", htmlentities($res[$k]->MessageBody, ENT_QUOTES, 'UTF-8'))."</td>";
+            echo "	<td scope=\"col\"><a href='DownloadFile.php?FileType=messages&RecID=".$res[$k]->MessageID."'><img src='images/Download.gif'></a></td>";
+            echo "	<td scope=\"col\"><a  target=_blank href='ShowMessagePhoto.php?MessageID=".$res[$k]->MessageID."'><img src='ShowMessagePhoto.php?MessageID=".$res[$k]->MessageID."' width=50></a></td>";
+            echo "	<td scope=\"col\">".$res[$k]->CreatorID_FullName."</td>";
+            echo "	<td scope=\"col\">".$res[$k]->CreateDate_Shamsi."</td>";
+            echo "	<td scope=\"col\">".$res[$k]->StartDate_Shamsi."</td>";
+            echo "	<td scope=\"col\">".$res[$k]->EndDate_Shamsi."</td>";
             echo "</tr>";
         }
         ?>
         <tr class="FooterOfTable">
             <td colspan="10" align="center">
-                <input type="button" onclick="javascript: ConfirmDelete();" value="<?php echo DELETE_M?>">
+                <input type="button"  class="btn btn-outline-danger" onclick="javascript: ConfirmDelete();" value="<?php echo DELETE_M?>">
             </td>
         </tr>
         <tr bgcolor="#cccccc"><td colspan="10" align="right">
-                <?
+                <?php
                 $TotalCount = manage_messages::SearchResultCount($MessageBody, $CreateDate, "");
                 for($k=0; $k<$TotalCount/$NumberOfRec; $k++)
                 {
@@ -348,9 +359,13 @@ if(isset($_REQUEST["SearchAction"]))
                 ?>
             </td></tr>
     </table>
+            </div>
 </form>
-<form target="_blank" method="post" action="Newmessages.php" id="NewRecordForm" name="NewRecordForm">
+    </div>
+</div>
+<!--<form target="_blank" method="post" action="Newmessages.php" id="NewRecordForm" name="NewRecordForm">
 </form>
+-->
 <script>
     function ConfirmDelete()
     {
@@ -368,4 +383,6 @@ if(isset($_REQUEST["SearchAction"]))
         SearchForm.submit();
     }
 </script>
+
+</body>
 </html>
