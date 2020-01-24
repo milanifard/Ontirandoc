1957af5387b4c991e97e442b592d0e6b0abd61bc
mghayour <me@mghayour.ir>
2020-01-24 20:11:15 +0330

    fix table

--- www/FormsGenerator/ManageFormsSections.php
+++ www/FormsGenerator/ManageFormsSections.php
@@ -57,72 +57,74 @@ if(isset($_REQUEST["UpdateID"]))
 }	
 ?>
 <form method="post" id="f1" name="f1" >
-<?
-	if(isset($_REQUEST["UpdateID"])) 
-	{
-		echo "<input type=\"hidden\" name=\"UpdateID\" id=\"UpdateID\" value='".$_REQUEST["UpdateID"]."'>";
-	}
-//echo manage_FormsStruct::ShowSummary($_REQUEST["FormsStructID"]);
-//echo manage_FormsStruct::ShowTabs($_REQUEST["FormsStructID"], "ManageFormsSections");
-?>
-<br><table width="90%" border="1" cellspacing="0" align="center">
-<tr class="HeaderOfTable">
-<td align="center">ایجاد/ویرایش بخش های فرم</td>
-</tr>
-<tr>
-<td>
-<table width="100%" border="0">
-<? 
-if(!isset($_REQUEST["UpdateID"]))
-{
-?> 
-<input type="hidden" name="FormsStructID" id="FormsStructID" value='<? if(isset($_REQUEST["FormsStructID"])) echo htmlentities($_REQUEST["FormsStructID"], ENT_QUOTES, 'UTF-8'); ?>'>
-<? } ?>
-<tr>
-	<td width="1%" nowrap>
- نام بخش
-	</td>
-	<td nowrap>
-	<input type="text" name="Item_SectionName" id="Item_SectionName" maxlength="250" size="40">
-	</td>
-</tr>
-<tr>
-	<td width="1%" nowrap>
- ترتیب نمایش
-	</td>
-	<td nowrap>
-	<input type="text" name="Item_ShowOrder" id="Item_ShowOrder" maxlength="2" size="2">
-	</td>
-</tr>
-<tr>
-	<td width="1%" nowrap>
- 	متن بالای بخش
-	</td>
-	<td nowrap>
-	<textarea name="Item_HeaderDesc" id="Item_HeaderDesc" cols="100" rows="5"><?php  if(isset($_REQUEST["UpdateID"])) echo $obj->HeaderDesc; ?></textarea>
-	</td>
-</tr>
-<tr>
-	<td width="1%" nowrap>
- 	متن پایین بخش
-	</td>
-	<td nowrap>
-	<textarea name="Item_FooterDesc" id="Item_FooterDesc" cols="100" rows="5"><?php  if(isset($_REQUEST["UpdateID"])) echo $obj->FooterDesc; ?></textarea>
-	</td>
-</tr>
+	<?
+		if(isset($_REQUEST["UpdateID"])) 
+		{
+			echo "<input type=\"hidden\" name=\"UpdateID\" id=\"UpdateID\" value='".$_REQUEST["UpdateID"]."'>";
+		}
+	//echo manage_FormsStruct::ShowSummary($_REQUEST["FormsStructID"]);
+	//echo manage_FormsStruct::ShowTabs($_REQUEST["FormsStructID"], "ManageFormsSections");
+	?>
+	<br>
+	<table width="90%" border="1" cellspacing="0" align="center">
+		<tr class="HeaderOfTable">
+		<td align="center">ایجاد/ویرایش بخش های فرم</td>
+		</tr>
+		<tr>
+		<td>
+		<table width="100%" border="0">
+			<? 
+			if(!isset($_REQUEST["UpdateID"]))
+			{
+			?> 
+			<input type="hidden" name="FormsStructID" id="FormsStructID" value='<? if(isset($_REQUEST["FormsStructID"])) echo htmlentities($_REQUEST["FormsStructID"], ENT_QUOTES, 'UTF-8'); ?>'>
+			<? } ?>
+			<tr>
+				<td width="1%" nowrap>
+			نام بخش
+				</td>
+				<td nowrap>
+				<input type="text" name="Item_SectionName" id="Item_SectionName" maxlength="250" size="40">
+				</td>
+			</tr>
+			<tr>
+				<td width="1%" nowrap>
+			ترتیب نمایش
+				</td>
+				<td nowrap>
+				<input type="text" name="Item_ShowOrder" id="Item_ShowOrder" maxlength="2" size="2">
+				</td>
+			</tr>
+			<tr>
+				<td width="1%" nowrap>
+				متن بالای بخش
+				</td>
+				<td nowrap>
+				<textarea name="Item_HeaderDesc" id="Item_HeaderDesc" cols="100" rows="5"><?php  if(isset($_REQUEST["UpdateID"])) echo $obj->HeaderDesc; ?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td width="1%" nowrap>
+				متن پایین بخش
+				</td>
+				<td nowrap>
+				<textarea name="Item_FooterDesc" id="Item_FooterDesc" cols="100" rows="5"><?php  if(isset($_REQUEST["UpdateID"])) echo $obj->FooterDesc; ?></textarea>
+				</td>
+			</tr>
 
-</table>
-</td>
-</tr>
-<tr class="FooterOfTable">
-<td align="center">
-<input type="button" onclick="javascript: ValidateForm();" value="ذخیره">
- <input type="button" onclick="javascript: document.location='ManageFormsSections.php?FormsStructID=<?php echo $_REQUEST["FormsStructID"]; ?>'" value="جدید">
-</td>
-</tr>
-</table>
-<input type="hidden" name="Save" id="Save" value="1">
-</form><script>
+		</table>
+		</td>
+		</tr>
+		<tr class="FooterOfTable">
+		<td align="center">
+		<input type="button" onclick="javascript: ValidateForm();" value="ذخیره">
+		<input type="button" onclick="javascript: document.location='ManageFormsSections.php?FormsStructID=<?php echo $_REQUEST["FormsStructID"]; ?>'" value="جدید">
+		</td>
+		</tr>
+	</table>
+	<input type="hidden" name="Save" id="Save" value="1">
+</form>
+<script>
 	<? echo $LoadDataJavascriptCode; ?>
 	function ValidateForm()
 	{
