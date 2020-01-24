beebbab362a0559d1c48cf351ed032eac1d6d7da
mghayour <me@mghayour.ir>
2020-01-24 20:08:14 +0330

    echo constants

--- www/pm/SearchTasks.php
+++ www/pm/SearchTasks.php
@@ -100,13 +100,13 @@ else
 		<div class="card">
 			<div class="card-header">
 				<i class="fa fa-search"></i>
-				<?php echo C_SEARCH ?>
+				<?php echo C_SEARCH; ?>
 			</div>
 			<div class="card-body">
 				<table class="table">
 					<tr>
 						<td nowrap>
-							C_RELATED_PROJECT
+							<?php echo C_RELATED_PROJECT; ?>
 						</td>
 						<td nowrap>
 						<select name="Item_ProjectID" id="Item_ProjectID">
@@ -117,7 +117,7 @@ else
 
 					<tr>
 						<td nowrap>
-							C_TASK_TYPE
+							<?php echo C_TASK_TYPE; ?>
 						</td>
 						<td nowrap>
 						<select name="Item_ProjectTaskTypeID" id="Item_ProjectTaskTypeID">
@@ -128,7 +128,7 @@ else
 
 					<tr>
 						<td nowrap>
-							C_TITLE
+							<?php echo C_TITLE; ?>
 						</td>
 						<td nowrap>
 						<input type="text" name="Item_title" id="Item_title" maxlength="1000" size="40">
@@ -137,7 +137,7 @@ else
 
 					<tr>
 						<td nowrap>
-							C_DESCRIPTION
+							<?php echo C_DESCRIPTION; ?>
 						</td>
 						<td nowrap>
 						<input type=text name="Item_description" id="Item_description" mexlength="1000" size="40">
@@ -146,7 +146,7 @@ else
 
 					<tr>
 						<td nowrap>
-							C_NOTE
+							<?php echo C_NOTE; ?>
 						</td>
 						<td nowrap>
 						<input type="text" name="Item_TaskComment" id="Item_TaskComment" maxlength="1000" size="40">
@@ -154,7 +154,7 @@ else
 					</tr>
 					<tr>
 						<td nowrap>
-							C_DOCUMENT
+							<?php echo C_DOCUMENT; ?>
 						</td>
 						<td nowrap>
 						<input type="text" name="Item_DocumentDescription" id="Item_DocumentDescription" maxlength="1000" size="40">
@@ -162,7 +162,7 @@ else
 					</tr>
 					<tr>
 						<td nowrap>
-							C_ACTION
+							<?php echo C_ACTION; ?>
 						</td>
 						<td nowrap>
 						<input type="text" name="Item_ActivityDescription" id="Item_ActivityDescription" maxlength="1000" size="40">
@@ -172,56 +172,56 @@ else
 
 					<tr>
 						<td nowrap>
-							C_PRIORITY
+							<?php echo C_PRIORITY; ?>
 						</td>
 						<td nowrap>
 						<select name="Item_TaskPeriority" id="Item_TaskPeriority" >
 							<option value=0>-
-							<option value='3'>C_PRIORITY_NORMAL</option>
-							<option value='4'>C_PRIORITY_LOW</option>
-							<option value='2'>C_PRIORITY_HIGH</option>
-							<option value='1'>C_PRIORITY_CRITICAL</option>
+							<option value='3'><?php echo C_PRIORITY_NORMAL; ?></option>
+							<option value='4'><?php echo C_PRIORITY_LOW; ?></option>
+							<option value='2'><?php echo C_PRIORITY_HIGH; ?></option>
+							<option value='1'><?php echo C_PRIORITY_CRITICAL; ?></option>
 						</select>
 						</td>
 					</tr>
 					<tr>
 						<td nowrap>
-							C_STATUS
+							<?php echo C_STATUS; ?>
 						</td>
 						<td nowrap>
 						<select name="Item_TaskStatus" id="Item_TaskStatus" >
 							<option value=0>-
-							<option value='NOT_START'>C_STATUS_NOT_START</option>
-							<option value='PROGRESSING'>C_STATUS_PROGRESSING</option>
-							<option value='DONE'>C_STATUS_DONE</option>
-							<option value='SUSPENDED'>C_STATUS_SUSPENDED</option>
-							<option value='REPLYED'>C_STATUS_REPLYED</option>
+							<option value='NOT_START'><?php echo C_STATUS_NOT_START; ?></option>
+							<option value='PROGRESSING'><?php echo C_STATUS_PROGRESSING; ?></option>
+							<option value='DONE'><?php echo C_STATUS_DONE; ?></option>
+							<option value='SUSPENDED'><?php echo C_STATUS_SUSPENDED; ?></option>
+							<option value='REPLYED'><?php echo C_STATUS_REPLYED; ?></option>
 						</select>
 						</td>
 					</tr>
 					<tr>
 						<td>
-							C_CREATOR
+							<?php echo C_CREATOR; ?>
 						</td>
 						<td>
 						<input type=hidden name="Item_CreatorID" id="Item_CreatorID" value=0>
 						<span id="Span_PersonID_FullName" name="Span_PersonID_FullName"></span> 	
-						<a href='#' onclick='javascript: window.open("SelectStaff.php?FormName=SearchForm&InputName=Item_CreatorID&SpanName=Span_PersonID_FullName");'>[C_SELECT]</a>
+						<a href='#' onclick='javascript: window.open("SelectStaff.php?FormName=SearchForm&InputName=Item_CreatorID&SpanName=Span_PersonID_FullName");'>[<?php echo C_SELECT; ?>]</a>
 						</td>
 					</tr>
 					<tr>
 						<td>
-							C_ECECUTOR / C_VIEWER
+							<?php echo C_ECECUTOR; ?> / <?php echo C_VIEWER; ?>
 						</td>
 						<td>
 						<input type=hidden name="Item_ExecutorID" id="Item_ExecutorID">
 						<span id="Span_Executor_FullName" name="Span_Executor_FullName"></span> 	
-						<a href='#' onclick='javascript: window.open("SelectStaff.php?FormName=SearchForm&InputName=Item_ExecutorID&SpanName=Span_Executor_FullName");'>[C_SELECT]</a>
+						<a href='#' onclick='javascript: window.open("SelectStaff.php?FormName=SearchForm&InputName=Item_ExecutorID&SpanName=Span_Executor_FullName");'>[<?php echo C_SELECT; ?>]</a>
 						</td>
 					</tr>
 				</table>
-				<input class="btn btn-primary" type="submit" value="C_SEARCH">
-				<input class="btn btn-light" type=button value='C_REMOVE_PREVIOUS_CHOICE' onclick='javascript: ClearLastSelected();'>
+				<input class="btn btn-primary" type="submit" value="<?php echo C_SEARCH; ?>">
+				<input class="btn btn-light" type=button value='<?php echo C_REMOVE_PREVIOUS_CHOICE; ?>' onclick='javascript: ClearLastSelected();'>
 			</div>
 		</div>
 	</div>
@@ -270,21 +270,21 @@ else
 		<div class="card">
 			<div class="card-header">
 				<i class="fa fa-file-alt"></i>
-				C_TASK
+				<?php echo C_TASK; ?>
 			</div>
 			<div class="card-body">
 				<table class="table table-striped">
 					<thead>
 						<th>
-							<td>C_ROW</td>
-							<td>C_MQ_CODE</td>
-							<td><a href="javascript: Sort('ProjectID', 'ASC');">C_RELATED_PROJECT</a></td>
-							<td><a href="javascript: Sort('ProjectTaskTypeID', 'ASC');">C_TASK_TYPE</a></td>
-							<td><a href="javascript: Sort('title', 'ASC');">C_TITLE</a></td>
-							<td><a href="javascript: Sort('title', 'ASC');">C_STATUS</a></td>
+							<td><?php echo C_ROW; ?></td>
+							<td><?php echo C_MQ_CODE; ?></td>
+							<td><a href="javascript: Sort('ProjectID', 'ASC');"><?php echo C_RELATED_PROJECT; ?></a></td>
+							<td><a href="javascript: Sort('ProjectTaskTypeID', 'ASC');"><?php echo C_TASK_TYPE; ?></a></td>
+							<td><a href="javascript: Sort('title', 'ASC');"><?php echo C_TITLE; ?></a></td>
+							<td><a href="javascript: Sort('title', 'ASC');"><?php echo C_STATUS; ?></a></td>
 							<td><a href="javascript: Sort('CreatorID', 'ASC');">CREATOR_M</a></td>
 							<td nowrap width=1%>
-							C_SELECT
+							<?php echo C_SELECT; ?>
 							</td>
 						</th>
 					</thead>
@@ -304,7 +304,7 @@ else
 							echo "	<td>".$res[$k]->CreatorID_FullName."</td>";
 							echo "<td nowrap>";
 							$FilteredTaskTitle = str_replace("\"", " ", str_replace("'", " ", $res[$k]->title));
-							echo "<a href=\"javascript: SelectTask(".$res[$k]->ProjectTaskID.", '".$res[$k]->ProjectTaskID.") ".$FilteredTaskTitle."')\">C_SELECT</a>";
+							echo "<a href=\"javascript: SelectTask(".$res[$k]->ProjectTaskID.", '".$res[$k]->ProjectTaskID.") ".$FilteredTaskTitle."')\"><?php echo C_SELECT; ?></a>";
 							echo "</td>";
 							echo "</tr>";
 						}
