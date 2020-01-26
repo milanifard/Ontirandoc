f95c3b178b9616b900b3771ccc5326b8b9ecf0cc
Javad <jd.mn98@yahoo.com>
2020-01-26 13:16:27 +0330

    ManageOntologyPropertyLabels.php bootstrap and font awesome icons

--- www/pm/ManageOntologyPropertyLabels.php
+++ www/pm/ManageOntologyPropertyLabels.php
@@ -44,37 +44,35 @@ if (isset($_REQUEST["UpdateID"])) {
 	echo manage_OntologyProperties::ShowTabs($_REQUEST["OntologyPropertyID"], "ManageOntologyPropertyLabels");
 	?>
 	<br>
-	<table width="90%" border="1" cellspacing="0" align="center">
-		<tr class="HeaderOfTable">
-			<td align="center"><? echo C_CREATE_EDIT_LABELS ?></td>
-		</tr>
-		<tr>
-			<td>
-				<table width="100%" border="0">
-					<?
-					if (!isset($_REQUEST["UpdateID"])) {
-					?>
-						<input type="hidden" name="OntologyPropertyID" id="OntologyPropertyID" value='<? if (isset($_REQUEST["OntologyPropertyID"])) echo htmlentities($_REQUEST["OntologyPropertyID"], ENT_QUOTES, 'UTF-8'); ?>'>
-					<? } ?>
-					<tr>
-						<td width="1%" nowrap>
-							<? echo C_LABEL ?>
-						</td>
-						<td nowrap>
-							<textarea name="Item_label" id="Item_label" cols="80" rows="5"><? echo $comment ?></textarea>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<tr class="FooterOfTable">
+	<div class="container">
+		<div class="row" style="margin-bottom: 10px">
+			<div class="col-12 text-center"><? echo C_CREATE_EDIT_LABELS ?></div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<?
+				if (!isset($_REQUEST["UpdateID"])) {
+				?>
+					<input type="hidden" name="OntologyPropertyID" id="OntologyPropertyID" value='<? if (isset($_REQUEST["OntologyPropertyID"])) echo htmlentities($_REQUEST["OntologyPropertyID"], ENT_QUOTES, 'UTF-8'); ?>'>
+				<? } ?>
+				<div class="row">
+					<div class="col-1" nowrap>
+						<? echo C_LABEL ?>
+					</div>
+					<div class="col-11" nowrap>
+						<textarea name="Item_label" id="Item_label" class="form-control" cols="80" rows="5"><? echo $comment ?></textarea>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center" style="margin-top: 10px">
 			<td align="center">
-				<input type="button" onclick="javascript: ValidateForm();" value="<? echo C_SAVE ?>">
-				<input type="button" onclick="javascript: document.location='ManageOntologyPropertyLabels.php?OntologyPropertyID=<?php echo $_REQUEST["OntologyPropertyID"]; ?>'" value="<? echo C_NEW ?>">
-				<input type="button" onclick="javascript: window.close();" value="<? echo C_CLOSE ?>">
+				<input type="button" class="btn btn-success mx-2" onclick="javascript: ValidateForm();" value="<? echo C_SAVE ?>">
+				<input type="button" class="btn btn-primary mx-2" onclick="javascript: document.location='ManageOntologyPropertyLabels.php?OntologyPropertyID=<?php echo $_REQUEST["OntologyPropertyID"]; ?>'" value="<? echo C_NEW ?>">
+				<input type="button" class="btn btn-danger mx-2" onclick="javascript: window.close();" value="<? echo C_CLOSE ?>">
 			</td>
-		</tr>
-	</table>
+		</div>
+	</div>
 	<input type="hidden" name="Save" id="Save" value="1">
 </form>
 <script>
