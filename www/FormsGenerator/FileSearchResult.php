0f0e99733256c5b71a8ed0f3700e522fe39c6ab7
Reza Latifi <reza.bubba@gmail.com>
2020-01-31 12:20:20 +0330

    security, performance, readability, bootstrap completed
    security: validate input functions added.
    performance: replaces if statements with dictionary.
    readability: replaced "for" with "for each" and if statements with switch.
    bootstrap: zebra-striped tables.

--- www/FormsGenerator/FileSearchResult.php
+++ www/FormsGenerator/FileSearchResult.php
@@ -6,32 +6,34 @@ include_once("classes/SecurityManager.class.php");
 include_once("classes/files.class.php");
 HTMLBegin();
 $list = SecurityManager::GetUserPermittedFileTypesForAccess($_SESSION["PersonID"]);
+$_REQUEST = SecurityManager::validateInput($_REQUEST);
 $FileTypeOptions = "";
-for($i=0; $i<count($list); $i++)
+$personTypes = ["STAFF" => "کارمند", "PROF" => "هیات علمی", "STUDENT" => "دانشجو", "OTHER" => "سایر"];
+foreach($list as $key => $item)
 {
-	if($i>0)
+	if($key>0)
 		$FileTypeOptions .= " or ";
-	$FileTypeOptions .= "(f.FileTypeID=".$list[$i]["FileTypeID"];
+	$FileTypeOptions .= "(f.FileTypeID=".$item["FileTypeID"];
 	// اگر دسترسی کاربر به این نوع دارای محدودیتی بود آن محدودیت هم به شرط اضافه می شود
-	if($list[$i]["AccessRange"]=="UNIT")
-		$FileTypeOptions .= " and f.ouid in (".$list[$i]["PermittedRangeList"].") ";
-	else if($list[$i]["AccessRange"]=="SUB_UNIT")
-		$FileTypeOptions .= " and f.sub_ouid in (".$list[$i]["PermittedRangeList"].")  ";
-	else if($list[$i]["AccessRange"]=="EDU_GROUP")
-		$FileTypeOptions .= " and f.EduGrpCode in (".$list[$i]["PermittedRangeList"].")  ";
-	else if($list[$i]["AccessRange"]=="ONLY_USER")
+	if($item["AccessRange"]=="UNIT")
+		$FileTypeOptions .= " and f.ouid in (".$item["PermittedRangeList"].") ";
+	else if($item["AccessRange"]=="SUB_UNIT")
+		$FileTypeOptions .= " and f.sub_ouid in (".$item["PermittedRangeList"].")  ";
+	else if($item["AccessRange"]=="EDU_GROUP")
+		$FileTypeOptions .= " and f.EduGrpCode in (".$item["PermittedRangeList"].")  ";
+	else if($item["AccessRange"]=="ONLY_USER")
 		$FileTypeOptions .= " and f.CreatorID='".$_SESSION["PersonID"]."' ";
 	$FileTypeOptions .= ")";
 }
-if($FileTypeOptions=="")
+if(empty($FileTypeOptions))
 {
 	echo "هیچ نوع پرونده ای در دسترس شما نمی باشد";
 	die();
 }
 $ListCondition = " (".$FileTypeOptions.") ";
-if($_REQUEST["FileType"] && $_REQUEST["FileTypeID"]!="0" && $_REQUEST["FileTypeID"]!="")
+if($_REQUEST["FileType"] && $_REQUEST["FileTypeID"]!="0" && !empty($_REQUEST["FileTypeID"]))
 	$ListCondition .= " and FileTypeID='".$_REQUEST["FileTypeID"]."' ";
-if($_REQUEST["FileID"] && $_REQUEST["FileID"]!="")
+if($_REQUEST["FileID"] && !empty($_REQUEST["FileID"]))
 	$ListCondition .= " and FileID='".$_REQUEST["FileID"]."' ";
 if($_REQUEST["ouid"] && $_REQUEST["ouid"]!="0")
 	$ListCondition .= " and f.ouid='".$_REQUEST["ouid"]."' ";
@@ -39,69 +41,59 @@ if($_REQUEST["sub_ouid"] && $_REQUEST["sub_ouid"]!="0")
 	$ListCondition .= " and f.sub_ouid='".$_REQUEST["sub_ouid"]."' ";
 if($_REQUEST["EduGrpCode"] && $_REQUEST["EduGrpCode"]!="0")
 	$ListCondition .= " and f.EduGrpCode='".$_REQUEST["EduGrpCode"]."' ";
-if($_REQUEST["FileNo"] && $_REQUEST["FileNo"]!="")
+if($_REQUEST["FileNo"] && !empty($_REQUEST["FileNo"]))
 	$ListCondition .= " and f.FileNo like '%".$_REQUEST["EduGrpCode"]."%' ";
 	
-if($_REQUEST["PLName"] && $_REQUEST["PLName"]!="")
+if($_REQUEST["PLName"] && !empty($_REQUEST["PLName"]))
 	$ListCondition .= " and (p.plname like '%".$_REQUEST["PLName"]."%' or s.PLName like '%".$_REQUEST["PLName"]."%' or f.PLName like '%".$_REQUEST["PLName"]."%')";
-if($_REQUEST["PFName"] && $_REQUEST["PFName"]!="")
+if($_REQUEST["PFName"] && !empty($_REQUEST["PFName"]))
 	$ListCondition .= " and (p.pfname like '%".$_REQUEST["PFName"]."%' or s.PFName like '%".$_REQUEST["PFName"]."%' or f.PFName like '%".$_REQUEST["PFName"]."%')";
 	
 //echo $ListCondition;
 
 $FileList = manage_files::GetList($ListCondition);
-echo "<br><table width=90% align=center cellspacing=0 cellpadding=3 border=1>";
-echo "<tr class=HeaderOfTable>";
-echo "<td width=1%>محتویات</td>";
-echo "<td width=1%>نوع</td>";
+echo "<br><table width='90%' align='center' cellspacing=0 cellpadding='3px' border='1px'>";
+echo "<tr class='HeaderOfTable'>";
+echo "<td width='1%'>محتویات</td>";
+echo "<td width='1%'>نوع</td>";
 echo "<td>عنوان</td>";
-echo "<td width=1%>شماره</td>";
-echo "<td width=1%>نوع فرد</td>";
-echo "<td width=1%>شخص مربوطه</td>";
+echo "<td width='1%'>شماره</td>";
+echo "<td width='1%'>نوع فرد</td>";
+echo "<td width='1%'>شخص مربوطه</td>";
 echo "<td>مکان</td>";
 echo "<td>امانت گیرندگان</td>";
 echo "</tr>";
-for($i=0; $i<count($FileList); $i++)
+foreach($FileList as $item)
 {
-	if($i%2==0)
-		echo "<tr class=OddRow>";
-	else
-		echo "<tr class=EvenRow>";
-	echo "<td nowrap><a href='NewFile.php?UpdateID=".$FileList[$i]->FileID."'><img border=0 width=30 src='images/edit.jpeg'></a></td>";
-	echo "<td nowrap>".$FileList[$i]->FileTypeName."</td>";
-	echo "<td nowrap>&nbsp;".$FileList[$i]->FileTitle."</td>";
-	echo "<td nowrap>&nbsp;".$FileList[$i]->FileNo."</td>";		
-	if($FileList[$i]->RelatedToPerson=="YES")
+	echo "<tr>";
+	echo "<td nowrap><a href='NewFile.php?UpdateID=".$item->FileID."'><img border=0 width=30 src='images/edit.jpeg'></a></td>";
+	echo "<td nowrap>".$item->FileTypeName."</td>";
+	echo "<td nowrap>&nbsp;".$item->FileTitle."</td>";
+	echo "<td nowrap>&nbsp;".$item->FileNo."</td>";		
+	if($item->RelatedToPerson==="YES")
 	{
 		echo "<td nowrap>&nbsp;";
-		if($FileList[$i]->PersonType=="STAFF")
-			echo "کارمند";
-		else if($FileList[$i]->PersonType=="PROF")
-			echo "هیات علمی";
-		else if($FileList[$i]->PersonType=="STUDENT")
-			echo "دانشجو";
-		else if($FileList[$i]->PersonType=="OTHER")
-			echo "سایر";
+		echo $personTypes[$item->PersonType];
 		echo "</td>";
-		echo "<td nowrap>&nbsp;".$FileList[$i]->PFName." ".$FileList[$i]->PLName."</td>";
+		echo "<td nowrap>&nbsp;".$item->PFName." ".$item->PLName."</td>";
 	}
 	else
 	{
-		echo "<td colspan=2>&nbsp;</td>";
+		echo "<td colspan='2'>&nbsp;</td>";
 	}
-	$Place = $FileList[$i]->UnitName;
-	if($FileList[$i]->SubUnitName!="")
+	$Place = $item->UnitName;
+	if(!empty($item->SubUnitName))
 	{
-		if($Place != "")
-			$Place .= " - ".$FileList[$i]->SubUnitName;
+		if(!empty($Place))
+			$Place .= " - ".$item->SubUnitName;
 	}
-	if($FileList[$i]->EduGrpName!="")
+	if(!empty($item->EduGrpName))
 	{
-		if($Place != "")
-			$Place .= " - ".$FileList[$i]->EduGrpName;
+		if(!empty($Place))
+			$Place .= " - ".$item->EduGrpName;
 	}
 	echo "<td nowrap>&nbsp;".$Place."</td>";
-	echo "<td>&nbsp;".$FileList[$i]->GetDeptedUsersName()."</td>";
+	echo "<td>&nbsp;".$item->GetDeptedUsersName()."</td>";
 	echo "</tr>";
 }
 echo "</table>";
