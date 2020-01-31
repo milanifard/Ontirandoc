c933c2df6a063c928fb87602ec509f4067213f6b
Reza Latifi <reza.bubba@gmail.com>
2020-01-31 11:13:36 +0330

    inpu validity check added

--- www/FormsGenerator/FillQuestionnaire.php
+++ www/FormsGenerator/FillQuestionnaire.php
@@ -11,6 +11,7 @@ include_once("classes/SecurityManager.class.php");
 //require_once('../organization/classes/ChartServices.class.php');
 HTMLBegin();
 $mysql = pdodb::getInstance();
+$_REQUEST = validateInput($_REQUEST);
 if(!isset($_REQUEST["SelectedFormStructID"]))
 {
 	echo "SelectedFormStructID=?";
@@ -22,13 +23,13 @@ if(!isset($_REQUEST["SelectedFormStructID"]))
 <br>
 <p align=center><span id=MessageSpan name=MessageSpan></span></p>
 <br>
-<?
+<?php
 	$CurForm = new be_FormsStruct();
 	$CurForm->LoadDataFromDatabase($_REQUEST["SelectedFormStructID"]);
 
 	$query = "select * from formsgenerator.TemporaryUsersAccessForms where WebUserID='".$_SESSION["UserID"]."' and FormsStructID=?";
 	$mysql->Prepare($query);
-	$res = $mysql->ExecuteStatement(array($_REQUEST["SelectedFormStructID"]));
+	$res = $mysql->ExecuteStatement([$_REQUEST["SelectedFormStructID"]]);
 	if($rec = $res->fetch())
 	{
 		if($rec["filled"]=="YES") // در حالتیکه پرسشنامه پر و تایید شده باشد
@@ -49,8 +50,7 @@ if(!isset($_REQUEST["SelectedFormStructID"]))
 				<input type=hidden name='SelectedFormStructID' id='SelectedFormStructID' value=0>
 			</form>
 			<script>
-				function ViewForm(FormsStructID, FormFlowStepID, RelatedRecordID)
-				{
+				function ViewForm(FormsStructID, FormFlowStepID, RelatedRecordID){
 					document.f2.FormFlowStepID.value=FormFlowStepID;
 					document.f2.RelatedRecordID.value=RelatedRecordID;
 					document.f2.SelectedFormStructID.value=FormsStructID;
@@ -68,54 +68,73 @@ if(!isset($_REQUEST["SelectedFormStructID"]))
 		die();
 	}
 	
-	if(isset($_REQUEST["ActionType"]))
-	{
-		// وقتی کاربر تایید نهایی کرده باشد فرم ارسال شده و دفعه بعدی که کاربر وارد شود نمی تواند آن را تغییر دهد
-		if($_REQUEST["ActionType"]=="CONFIRM")
-		{
-			$mysql->Prepare("update formsgenerator.TemporaryUsersAccessForms set filled='YES' where WebUserID='".$_SESSION["UserID"]."' and FormsStructID=?");
-			$mysql->ExecuteStatement(array($_REQUEST["SelectedFormStructID"]));
-		}
-		
-		if($_REQUEST["ActionType"]=="CONFIRM")
-		{
-			$RecID = $CurForm->AddData(-1, $_SESSION["PersonID"]);
+	if(isset($_REQUEST["ActionType"])){
+		switch($_REQUEST["ActionType"]){
+			case "CONFIRM":{
+				// وقتی کاربر تایید نهایی کرده باشد فرم ارسال شده و دفعه بعدی که کاربر وارد شود نمی تواند آن را تغییر دهد
+				$mysql->Prepare("update formsgenerator.TemporaryUsersAccessForms set filled='YES' where WebUserID='".$_SESSION["UserID"]."' and FormsStructID=?");
+				$mysql->ExecuteStatement([$_REQUEST["SelectedFormStructID"]]);
+				
+				$RecID = $CurForm->AddData(-1, $_SESSION["PersonID"]);
 
-			$mysql->Prepare("insert into formsgenerator.QuestionnairesCreators (UserID, FormsStructID, RelatedRecordID) values('".$_SESSION["UserID"]."', ?, '".$RecID."')");
-			$mysql->ExecuteStatement(array($_REQUEST["SelectedFormStructID"]));
-			
-			echo "<script>document.getElementById('MessageSpan').innerHTML='<font color=green>اطلاعات ذخیره شد</font>';</script>";
-			//echo "<script>setTimeout(\"document.getElementById('MessageSpan').innerHTML='';\", 1500);</script>";
-			echo "<p align=center><a href='login.php'>خروج از سیستم</a></p>";
-			die();
+				$mysql->Prepare("insert into formsgenerator.QuestionnairesCreators (UserID, FormsStructID, RelatedRecordID) values('".$_SESSION["UserID"]."', ?, '".$RecID."')");
+				$mysql->ExecuteStatement(array($_REQUEST["SelectedFormStructID"]));
+				
+				echo "<script>document.getElementById('MessageSpan').innerHTML='<font color=green>اطلاعات ذخیره شد</font>';</script>";
+				//echo "<script>setTimeout(\"document.getElementById('MessageSpan').innerHTML='';\", 1500);</script>";
+				echo "<p align=center><a href='login.php'>خروج از سیستم</a></p>";
+				die();
+			break;
+			}
+			case "SAVE_GO_EDIT":
+			case "SEND":{
+				$RecID = $CurForm->AddData(-1, $_SESSION["PersonID"]);
+				$CurForm->SendData($RecID, 0, -1, $_SESSION["PersonID"]);
+				$mysql->Prepare("insert into formsgenerator.QuestionnairesCreators (UserID, FormsStructID, RelatedRecordID) values('".$_SESSION["UserID"]."', ?, '".$RecID."')");
+				$mysql->ExecuteStatement([$_REQUEST["SelectedFormStructID"]]);
+				
+				?>
+				<form id=f2 name=f2 method=post action='ViewQuestionnaire.php'>
+					<input type=hidden name='FormFlowStepID' id='FormFlowStepID' value=0>
+					<input type=hidden name='RelatedRecordID' id='RelatedRecordID' value=0>
+					<input type=hidden name='SelectedFormStructID' id='SelectedFormStructID' value=0>
+				</form>
+				<script>
+					function ViewForm(FormsStructID, FormFlowStepID, RelatedRecordID)
+					{
+						document.f2.FormFlowStepID.value=FormFlowStepID;
+						document.f2.RelatedRecordID.value=RelatedRecordID;
+						document.f2.SelectedFormStructID.value=FormsStructID;
+						f2.submit();
+					}
+					ViewForm(<?php echo $CurForm->FormsStructID ?>, -1, <?php echo $RecID ?>);
+				</script>
+				<?php
+			break;
+			}
 		}
-		else if($_REQUEST["ActionType"]=="SAVE_GO_EDIT" || $_REQUEST["ActionType"]=="SEND")
-		{
-			$RecID = $CurForm->AddData(-1, $_SESSION["PersonID"]);
-			$CurForm->SendData($RecID, 0, -1, $_SESSION["PersonID"]);
-			$mysql->Prepare("insert into formsgenerator.QuestionnairesCreators (UserID, FormsStructID, RelatedRecordID) values('".$_SESSION["UserID"]."', ?, '".$RecID."')");
-			$mysql->ExecuteStatement(array($_REQUEST["SelectedFormStructID"]));
-			
-			?>
-			<form id=f2 name=f2 method=post action='ViewQuestionnaire.php'>
-				<input type=hidden name='FormFlowStepID' id='FormFlowStepID' value=0>
-				<input type=hidden name='RelatedRecordID' id='RelatedRecordID' value=0>
-				<input type=hidden name='SelectedFormStructID' id='SelectedFormStructID' value=0>
-			</form>
-			<script>
-				function ViewForm(FormsStructID, FormFlowStepID, RelatedRecordID)
-				{
-					document.f2.FormFlowStepID.value=FormFlowStepID;
-					document.f2.RelatedRecordID.value=RelatedRecordID;
-					document.f2.SelectedFormStructID.value=FormsStructID;
-					f2.submit();
-				}
-				ViewForm(<?php echo $CurForm->FormsStructID ?>, -1, <?php echo $RecID ?>);
-			</script>
-		<?php 
-		}
-}
+	}
 echo $CurForm->CreateUserInterface(-1, $_SESSION["PersonID"], 0);
 	echo "<p align=center><input type=button value='خروج از سیستم' onclick='javascript: document.location=\"login.php?logout=1\"'></p>";
+
+	// prevent injection attacks
+	function validateInput(array $input):array{
+		foreach($input as $key => $value){
+			$input[$key] = validateText($value);
+		}
+		return $input;
+	}
+
+	function validateText($str):string{
+    if (is_object($str) || is_array($str)) {
+        return '';
+    }
+    $filtered = iconv('UTF-8//IGNORE', "ISO-8859-1//IGNORE", (string) $str); // check if chrarachters are in UTF-8
+		$filtered = htmlentities($filtered);
+		$filtered = htmlspecialchars($filtered);
+		$filtered = preg_replace('/[\r\n\t ]+/', ' ', $filtered); // ignore white spaces
+    $filtered = trim($filtered);
+    return $filtered;
+	}
 ?>
 </html>
